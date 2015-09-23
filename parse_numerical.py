c195b90733e7a24993e2b5881393d19efe4f3cc0
Yair Beer <yaia1309@gmail.com>
2015-09-23 18:13:20 +0300

    remove duplicate lines (for low memory)

--- parse_numerical.py
+++ parse_numerical.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as plt
 
 dataset = pd.DataFrame.from_csv("train.csv")
 print dataset.head()
+col_n = dataset.shape[1]
 
-dataset = dataset.T
-dataset = dataset.drop_duplicates()
-dataset = dataset.T
-print dataset.head()
+col_names = dataset.columns.values.tolist()
+col_types = dataset.dtypes
 
-# col_names = dataset.columns.values.tolist()
-#
-# col_types = dataset.dtypes
+dataset_temp = dataset[col_names[:100]]
+print dataset_temp.shape
+
+dataset_temp = dataset_temp.T.drop_duplicates().T
+print dataset_temp.shape
+
 #
 # for i, col in enumerate(col_names[40:100]):
 #     if col_types[i] == 'int64' or col_types[i] == 'float64':

99a8ed5ea4f885dfa949e63478cf815f06dbac99
Yair Beer <yaia1309@gmail.com>
2015-09-24 10:46:59 +0300

    merged, remove dupes

--- parsing.py
+++ parsing.py
@@ -51,170 +51,168 @@ Remove comlumns with only 1 answer
 """
 Remove duplicate comlumns
 """
-dataset = pd.DataFrame.from_csv("train_col_filt.csv")
-print dataset.head()
-
-col_n = dataset.shape[1]
-interval = 500
-
-col_names = dataset.columns.values.tolist()
-col_types = dataset.dtypes
-
-dataset_splited = []
-for i in range(0, col_n, interval):
-    if col_n > i + interval:
-        dataset_temp = dataset[col_names[i: (i + interval)]]
-        print i, ' before: ', dataset_temp.shape
-
-        dataset_temp = dataset_temp.T.drop_duplicates().T
-        print i, ' after: ', dataset_temp.shape
-        dataset_splited.append(dataset_temp)
-    else:
-        dataset_temp = dataset[col_names[i:]]
-        print i, ' before: ', dataset_temp.shape
-
-        dataset_temp = dataset_temp.T.drop_duplicates().T
-        print i, ' after: ', dataset_temp.shape
-        dataset_splited.append(dataset_temp)
-dataset = pd.concat(dataset_splited, axis=1)
-
-del dataset_splited, dataset_temp
-print dataset.head()
-
-col_names = dataset.columns.values.tolist()
-
-dataset = pd.DataFrame.to_csv("train_col_filt_2.csv")
-
-dataset_test = pd.DataFrame.from_csv("test_col_filt.csv")
-dataset_test = dataset_test[col_names[:-1]]
-dataset_test.to_csv("test_col_filt_2.csv")
+# dataset = pd.DataFrame.from_csv("train_col_filt.csv")
+#
+# col_n = dataset.shape[1]
+# interval = 500
+#
+# col_names = dataset.columns.values.tolist()
+# col_types = dataset.dtypes
+#
+# dataset_splited = []
+# for i in range(0, col_n, interval):
+#     if col_n > i + interval:
+#         dataset_temp = dataset[col_names[i: (i + interval)]]
+#         print i, ' before: ', dataset_temp.shape
+#
+#         dataset_temp = dataset_temp.T.drop_duplicates().T
+#         print i, ' after: ', dataset_temp.shape
+#         dataset_splited.append(dataset_temp)
+#     else:
+#         dataset_temp = dataset[col_names[i:]]
+#         print i, ' before: ', dataset_temp.shape
+#
+#         dataset_temp = dataset_temp.T.drop_duplicates().T
+#         print i, ' after: ', dataset_temp.shape
+#         dataset_splited.append(dataset_temp)
+# dataset = pd.concat(dataset_splited, axis=1)
+#
+# del dataset_splited, dataset_temp
+#
+# col_names = dataset.columns.values.tolist()
+#
+# dataset.to_csv("train_col_filt_2.csv")
+#
+# dataset_test = pd.DataFrame.from_csv("test_col_filt.csv")
+# dataset_test = dataset_test[col_names[:-1]]
+# dataset_test.to_csv("test_col_filt_2.csv")
 
 """
 change categorical variables to dummy variables, meanwhile ignoring variables with more than 20 values
 """
-# get file with only relevant rows
-print 'reading train dataset'
-dataset = pd.DataFrame.from_csv("train_col_filt_2.csv")
-
-print 'reading test dataset'
-dataset_test = pd.DataFrame.from_csv("test_col_filt_2.csv")
-
-good_columns = list(dataset.columns.values)
-
-# check column data type
-data_types = dataset.dtypes
-dummies = []
-dummies_test = []
-print 'starting to convert to dummy variables'
-for i in range(len(good_columns) - 1):
-    # use getdummies in order to convert categorial to workable numerical table
-    col_dif_values = dataset[good_columns[i]].value_counts().shape[0]
-    # maximum number of columns viable to create dummies
-    print good_columns[i], ' has ', col_dif_values, ' columns'
-    if data_types[i] == 'object':
-        if col_dif_values <= 100:
-            print 'working'
-            new_dummy = pd.get_dummies(dataset[good_columns[i]]).astype('float64')
-            new_dummy_test = pd.get_dummies(dataset_test[good_columns[i]]).astype('float64')
-            columns_dummy = new_dummy.columns.values.tolist()
-            for j in range(len(columns_dummy)):
-                columns_dummy[j] = good_columns[i] + '_' + str(columns_dummy[j])
-            new_dummy.columns = columns_dummy
-            # remove categorical column
-            dummies.append(new_dummy)
-            dummies_test.append(new_dummy_test)
-            # add dummy columns
-        else:
-            print dataset[good_columns[i]].value_counts().shape[0]
-            print dataset[good_columns[i]].value_counts().index[:5]
-        dataset = dataset.drop(good_columns[i], 1)
-        dataset_test = dataset_test.drop(good_columns[i], 1)
-dataset = pd.concat(dummies + [dataset], axis=1)
-dataset_test = pd.concat(dummies_test + [dataset_test], axis=1)
-
-columns_dummy = dataset.columns.values.tolist()
-columns_dummy_test = dataset_test.columns.values.tolist()
-
-columns_dummy_and = []
-for col in columns_dummy:
-    if col in columns_dummy_test:
-        columns_dummy_and.append(col)
-dataset = dataset[columns_dummy_and + ['target']]
-dataset_test = dataset_test[columns_dummy_and]
-
-print 'added new ', len(columns_dummy), ' columns'
-
-print 'finished converting dummies'
-
-dataset.to_csv("train_col_dummy.csv")
-dataset_test.to_csv("test_col_dummy.csv")
-
-print 'written dataframe with str to dummy to file'
+# # get file with only relevant rows
+# print 'reading train dataset'
+# dataset = pd.DataFrame.from_csv("train_col_filt_2.csv")
+#
+# print 'reading test dataset'
+# dataset_test = pd.DataFrame.from_csv("test_col_filt_2.csv")
+#
+# good_columns = list(dataset.columns.values)
+#
+# # check column data type
+# data_types = dataset.dtypes
+# dummies = []
+# dummies_test = []
+# print 'starting to convert to dummy variables'
+# for i in range(len(good_columns) - 1):
+#     # use getdummies in order to convert categorial to workable numerical table
+#     col_dif_values = dataset[good_columns[i]].value_counts().shape[0]
+#     # maximum number of columns viable to create dummies
+#     print good_columns[i], ' has ', col_dif_values, ' columns'
+#     if data_types[i] == 'object':
+#         if col_dif_values <= 100:
+#             print 'working'
+#             new_dummy = pd.get_dummies(dataset[good_columns[i]]).astype('float64')
+#             new_dummy_test = pd.get_dummies(dataset_test[good_columns[i]]).astype('float64')
+#             columns_dummy = new_dummy.columns.values.tolist()
+#             for j in range(len(columns_dummy)):
+#                 columns_dummy[j] = good_columns[i] + '_' + str(columns_dummy[j])
+#             new_dummy.columns = columns_dummy
+#             # remove categorical column
+#             dummies.append(new_dummy)
+#             dummies_test.append(new_dummy_test)
+#             # add dummy columns
+#         else:
+#             print dataset[good_columns[i]].value_counts().shape[0]
+#             print dataset[good_columns[i]].value_counts().index[:5]
+#         dataset = dataset.drop(good_columns[i], 1)
+#         dataset_test = dataset_test.drop(good_columns[i], 1)
+# dataset = pd.concat(dummies + [dataset], axis=1)
+# dataset_test = pd.concat(dummies_test + [dataset_test], axis=1)
+#
+# columns_dummy = dataset.columns.values.tolist()
+# columns_dummy_test = dataset_test.columns.values.tolist()
+#
+# columns_dummy_and = []
+# for col in columns_dummy:
+#     if col in columns_dummy_test:
+#         columns_dummy_and.append(col)
+# dataset = dataset[columns_dummy_and + ['target']]
+# dataset_test = dataset_test[columns_dummy_and]
+#
+# print 'added new ', len(columns_dummy), ' columns'
+#
+# print 'finished converting dummies'
+#
+# dataset.to_csv("train_col_dummy.csv")
+# dataset_test.to_csv("test_col_dummy.csv")
+#
+# print 'written dataframe with str to dummy to file'
 
 """
 preprocessing pipe for univariante results
 """
-# get file with all numerics
-print 'loading dataset with dummies from file'
-dataset = pd.DataFrame.from_csv("train_col_dummy.csv")
-
-print 'changing to array'
-dataset = np.array(dataset)
-
-X = dataset[:, :-1]
-y = dataset[:, -1]
-
-# impotate
-print 'impotating'
-imp = Imputer(missing_values='NaN', strategy='mean', axis=1)
-imp.fit(X)
-X = imp.transform(X)
-
-# standardizing results
-print 'standardizing results'
-scaler = preprocessing.StandardScaler().fit(X)
-X = scaler.transform(X)
+# # get file with all numerics
+# print 'loading dataset with dummies from file'
+# dataset = pd.DataFrame.from_csv("train_col_dummy.csv")
+#
+# print 'changing to array'
+# dataset = np.array(dataset)
+#
+# X = dataset[:, :-1]
+# y = dataset[:, -1]
+#
+# # impotate
+# print 'impotating'
+# imp = Imputer(missing_values='NaN', strategy='mean', axis=1)
+# imp.fit(X)
+# X = imp.transform(X)
+#
+# # standardizing results
+# print 'standardizing results'
+# scaler = preprocessing.StandardScaler().fit(X)
+# X = scaler.transform(X)
 
 """
 univariante evaluation
 """
-# CV
-cv_n = 4
-kf = KFold(dataset.shape[0], n_folds=cv_n, shuffle=True)
-
-print 'start univariante evaluation'
-X_train_list = []
-X_test_list = []
-y_train_list = []
-y_test_list = []
-for train_index, test_index in kf:
-    X_train, X_test = X[train_index, :], X[test_index, :]
-    y_train, y_test = y[train_index].ravel(), y[test_index].ravel()
-    X_train_list.append(X_train)
-    X_test_list.append(X_test)
-    y_train_list.append(y_train)
-    y_test_list.append(y_test)
-
-uni_results = np.ones((dataset.shape[1], cv_n))
-for i in range(X.shape[1]):
-    if not i % 100:
-        print 'var ', i
-    for j in range(cv_n):
-        # train machine learning
-        classifier.fit(X_train_list[j][:, i].reshape((X_train_list[j].shape[0], 1)), y_train_list[j])
-
-        # predict
-        class_pred = classifier.predict_proba(X_test_list[j][:, i].reshape((X_test_list[j].shape[0], 1)))[:, 1]
-        # evaluate
-        uni_results[i, j] = roc_auc_score(y_test_list[j], class_pred)
-
-print uni_results
-print np.mean(uni_results, axis=1)
-uni_results = np.mean(uni_results, axis=1)
-
-uni_results = pd.Series(uni_results)
-print uni_results.value_counts()
-uni_results.to_csv("univar_AUC.csv")
+# # CV
+# cv_n = 4
+# kf = KFold(dataset.shape[0], n_folds=cv_n, shuffle=True)
+#
+# print 'start univariante evaluation'
+# X_train_list = []
+# X_test_list = []
+# y_train_list = []
+# y_test_list = []
+# for train_index, test_index in kf:
+#     X_train, X_test = X[train_index, :], X[test_index, :]
+#     y_train, y_test = y[train_index].ravel(), y[test_index].ravel()
+#     X_train_list.append(X_train)
+#     X_test_list.append(X_test)
+#     y_train_list.append(y_train)
+#     y_test_list.append(y_test)
+#
+# uni_results = np.ones((dataset.shape[1], cv_n))
+# for i in range(X.shape[1]):
+#     if not i % 100:
+#         print 'var ', i
+#     for j in range(cv_n):
+#         # train machine learning
+#         classifier.fit(X_train_list[j][:, i].reshape((X_train_list[j].shape[0], 1)), y_train_list[j])
+#
+#         # predict
+#         class_pred = classifier.predict_proba(X_test_list[j][:, i].reshape((X_test_list[j].shape[0], 1)))[:, 1]
+#         # evaluate
+#         uni_results[i, j] = roc_auc_score(y_test_list[j], class_pred)
+#
+# print uni_results
+# print np.mean(uni_results, axis=1)
+# uni_results = np.mean(uni_results, axis=1)
+#
+# uni_results = pd.Series(uni_results)
+# print uni_results.value_counts()
+# uni_results.to_csv("univar_AUC.csv")
 
 """
 use only columns over threshhold
@@ -222,7 +220,7 @@ use only columns over threshhold
 print 'loading univariante results'
 uni_results = pd.read_csv("univar_AUC.csv", index_col=0, names=["index", "AUC"])
 
-uni_thresh = 0.53
+uni_thresh = 0.55
 
 regression_matrix_indices = []
 for i in range(len(uni_results) - 1):
@@ -293,6 +291,6 @@ class_pred = classifier_full.predict_proba(X_test)[:, 1]
 
 submission_file = pd.DataFrame.from_csv("sample_submission.csv")
 submission_file['target'] = class_pred
-submission_file.to_csv("more_variantes_" + str(uni_thresh) + ".csv")
+submission_file.to_csv("rf_univar_" + str(uni_thresh) + ".csv")
 
 # date: 13JAN12:00:00:00

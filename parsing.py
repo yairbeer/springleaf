5e7efbf77beb0affa90fc8de258222a30d601c33
Yair Beer <yaia1309@gmail.com>
2015-09-24 13:22:17 +0300

    merged, remove dupes

--- parsing.py
+++ parsing.py
@@ -3,11 +3,12 @@ __author__ = 'YBeer'
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.cross_validation import KFold
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import Imputer
+from sklearn.decomposition import PCA
+
 
 classifier = RandomForestClassifier()
 classifier_full = GradientBoostingClassifier(loss='deviance', learning_rate=0.2, n_estimators=150, max_depth=3,
@@ -220,7 +221,7 @@ use only columns over threshhold
 print 'loading univariante results'
 uni_results = pd.read_csv("univar_AUC.csv", index_col=0, names=["index", "AUC"])
 
-uni_thresh = 0.57
+uni_thresh = 0.53
 
 regression_matrix_indices = []
 for i in range(len(uni_results) - 1):
@@ -249,30 +250,37 @@ print 'standardizing results'
 scaler = preprocessing.StandardScaler().fit(X)
 X = scaler.transform(X)
 
+# PCA
+# print 'PCA results'
+# pca_decomp = PCA(n_components=100)
+# X = pca_decomp.fit_transform(X)
+# print X.shape
+
 """
 full model CV
 """
-# CV
-cv_n = 4
-kf = KFold(dataset.shape[0], n_folds=cv_n, shuffle=True)
-
-print 'start full model evaluation'
-for train_index, test_index in kf:
-    X_train, X_test = X[train_index, :], X[test_index, :]
-    y_train, y_test = y[train_index].ravel(), y[test_index].ravel()
-
-    # train machine learning
-    classifier_full.fit(X_train, y_train)
-
-    # predict
-    class_pred = classifier_full.predict_proba(X_test)[:, 1]
-
-    # evaluate
-    print 'auc is: ', roc_auc_score(y_test, class_pred)
+# # CV
+# cv_n = 4
+# kf = KFold(dataset.shape[0], n_folds=cv_n, shuffle=True)
+#
+# print 'start full model evaluation'
+# for train_index, test_index in kf:
+#     X_train, X_test = X[train_index, :], X[test_index, :]
+#     y_train, y_test = y[train_index].ravel(), y[test_index].ravel()
+#
+#     # train machine learning
+#     classifier_full.fit(X_train, y_train)
+#
+#     # predict
+#     class_pred = classifier_full.predict_proba(X_test)[:, 1]
+#
+#     # evaluate
+#     print 'auc is: ', roc_auc_score(y_test, class_pred)
 
 """
 Evaluate test file
 """
+print 'fitting full data'
 # fitting full model
 X_train = X
 y_train = y
@@ -285,6 +293,7 @@ X_test = dataset_test[:, regression_matrix_indices]
 # preprocess
 X_test = imp.transform(X_test)
 X_test = scaler.transform(X_test)
+# X_test = PCA.transform(X_test)
 
 # predict
 class_pred = classifier_full.predict_proba(X_test)[:, 1]
